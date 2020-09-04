7ab9bf09944a668e996d94603ed4475e2c09986b
Dennis Kroné <dennis.krone@svenskaspel.se>
2020-09-04 16:51:52 +0200

    fix errors in site.py

--- ssg/site.py
+++ ssg/site.py
@@ -4,12 +4,12 @@ from pathlib import Path
 
 class Site:
     def __init__(self, source, dest):
-        self.path = Path(source)
+        self.source = Path(source)
         self.dest = Path(dest)
 
     def create_dir(self, path):
         directory = Path(f"{self.dest}/{path.relative_to(self.source)}")
-        directory.mkdir(parents=True, exist_ok=False)
+        directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
